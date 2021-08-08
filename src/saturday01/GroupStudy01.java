435481ee5178b3dddfd0eeb46c303c6d62d117d1
Adil Demirleng <demirleng@gmail.com>
2021-08-07 23:01:17 -0400

    new addition

--- src/saturday01/GroupStudy01.java
+++ src/saturday01/GroupStudy01.java
@@ -2,7 +2,8 @@ package saturday01;
 
 public class GroupStudy01 {
     public static void main(String[] args) {
+        //I changed some code in Github
+        System.out.println("Hey Github How are you?");
 
-        System.out.println("Hey Github");
     }
 }
