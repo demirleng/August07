7f30644e38da4eae49a3346cd32cc09370946a5c
demirleng <88108454+demirleng@users.noreply.github.com>
2021-08-07 22:57:11 -0400

    Update GroupStudy01.java

--- src/saturday01/GroupStudy01.java
+++ src/saturday01/GroupStudy01.java
@@ -4,5 +4,8 @@ public class GroupStudy01 {
     public static void main(String[] args) {
 
         System.out.println("Hey Github");
+        System.out.println("Hey Java!")
+        System.out.println("Hey IntelliJ!")
+        System.out.println("Hey Eclipse!")
     }
 }
